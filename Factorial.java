22b72c1689a29c9597176eedc5ef1e25ba76019b
GovindRaghav <35306643+GovindRaghav@users.noreply.github.com>
2018-01-27 10:38:30 +0530

    Factorial

---
+++ Factorial.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+class Factorial{
+	public static void main(String args[])
+	{
+		Scanner sc=new Scanner(System.in);
+		int n;
+		System.out.println("Enter number:");
+		n=sc.nextInt();
+		int i=n;
+		long factorl=1;
+		while(i>0)
+		{
+			factorl=factorl*i;
+			i--;
+		}
+		System.out.println("Factorial of"+n+"is="+factorl);
+	}
+}
